04152d919dc33870979721ca524f78a48edc24bd
zhangxiaogui <zxglive2006@126.com>
2019-02-09 21:59:37 +0800

    补充15章代码数据

--- ch15.py
+++ ch15.py
@@ -10,3 +10,8 @@ x = [10.1, 10, 9.8, 10.5, 9.7, 10.1, 9.9, 10.2, 10.3, 9.9]
 print(stats.t.interval(0.95, len(x) - 1, np.mean(x), stats.sem(x)))
 SHindex = pd.read_csv(r".\data\TRD_Index.csv")
 print(SHindex.head(3))
+
+TRD_Index = pd.read_csv(r".\data\TRd_Index.txt", sep="\t")
+SHindex = TRD_Index[TRD_Index.Indexcd==1]
+SHRet = SHindex.Retindex
+print(SHRet.head())
