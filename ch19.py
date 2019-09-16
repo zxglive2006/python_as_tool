25f1ed4fac5f69080fa09ed20b6ab02da19d53a4
zhangxiaogui <zxglive2006@126.com>
2019-09-16 18:00:53 +0800

    完善19章内容，增加20章内容

--- ch19.py
+++ ch19.py
@@ -4,6 +4,7 @@ import pandas as pd
 import math
 import matplotlib.pyplot as plt
 from scipy import linalg
+import ffn
 
 
 # 设置DataFrame显示宽度和列数
@@ -30,9 +31,50 @@ class MeanVariance:
         """
         covs = np.array(self.returns.cov())
         means = np.array(self.returns.mean())
-        L1 = np.append(np.append(covs.swapaxes(0, 1), [means], 0), [np.ones(len(means))], 0).swapaxes(0, 1)
+        ones = np.ones(len(means))
+        zeros = np.zeros(len(means))
+        temp = np.append(covs, [means], 0)
+        temp2 = np.append(temp, [ones], 0)
+        L1 = temp2.swapaxes(0, 1)
+        L2 = list(ones)
+        L2.extend([0, 0])
+        L3 = list(means)
+        L3.extend([0, 0])
+        L4 = np.array([L2, L3])
+        L = np.append(L1, L4, 0)
+        temp3 = np.append(zeros, [1, goalRet], 0)
+        results = linalg.solve(L, temp3)
+        return np.array([list(self.returns.columns), results[:-2]])
 
+    def meanRet(self, fracs):
+        """
+        给定各资产的比例，计算收益率均值
+        :param fracs:
+        :return:
+        """
+        meanRisky = ffn.to_returns(self.returns).mean()
+        assert len(meanRisky) == len(fracs)     # Length of fractions must be equal to naumber of assets
+        return np.sum(np.multiply(meanRisky, np.array(fracs)))
 
+    def calVar(self, fraces):
+        """
+        给定各资产的比例，计算收益率方差
+        :param fraces:
+        :return:
+        """
+        return np.dot(np.dot(fraces, self.returns.cov()), fraces)
+
+    def cal_func(self, x):
+        return self.calVar(self.minVar(x)[1, :].astype(np.float))
+
+    def frontierCurve(self):
+        """
+        定义绘制最小方差边缘曲线函数
+        :return:
+        """
+        goals = [x/50000 for x in range(-100, 4000)]
+        variances = list(map(self.cal_func, goals))
+        plt.plot(variances, goals)
 
 
 def cal_mean(frac):
@@ -84,14 +126,17 @@ def cal_corr():
     sh_return = pd.concat([byjc, fjgs, hxyh, sykj, zndl], axis=1)
     print(sh_return.head())
     sh_return = sh_return.dropna()
-    cumreturn = (1 + sh_return).cumprod()
+    print(sh_return.corr())
+    minVar = MeanVariance(sh_return)
+    # minVar.frontierCurve()
+    print(minVar.minVar(0.003))
+    # cumreturn = (1 + sh_return).cumprod()
     # sh_return.plot()
     # plt.title("Daily Return of 5 Stocks(2014-2015)")
     # plt.legend(loc="lower center", ncol=5, fancybox=True, shadow=True)
     # plt.show()
     # cumreturn.plot()
     # plt.show()
-    print(sh_return.corr())
 
 
 if __name__ == '__main__':
---
+++ ch20.py
@@ -0,0 +1,42 @@
+# encoding: UTF-8
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+
+# 设置DataFrame显示宽度和列数
+pd.set_option("display.width", 200)
+pd.set_option('display.max_columns', 16)
+
+
+if __name__ == '__main__':
+    print("ch20")
+    indexcd = pd.read_csv(r"./data/020/TRD_Index.csv", index_col="Trddt")
+    mktcd = indexcd[indexcd.Indexcd==902]
+    print(mktcd.head())
+    mktret = pd.Series(mktcd.Retindex.values, index=pd.to_datetime(mktcd.index))
+    mktret.name = "mktret"
+    print(mktret.head())
+    mktret = mktret["2014-01-02":"2014"]
+    print(mktret.tail())
+    xin_an = pd.read_csv(r"./data/020/xin_an.csv", index_col="Date")
+    xin_an.index = pd.to_datetime(xin_an.index)
+    print(xin_an.head())
+    xin_an = xin_an[xin_an.Volume != 0]
+    xin_anret = (xin_an.Close - xin_an.Close.shift(1)) / xin_an.Close.shift(1)
+    xin_anret.name = "returns"
+    xin_anret = xin_anret.dropna()
+    print(xin_anret.head())
+    print(xin_anret.tail())
+    Ret = pd.merge(pd.DataFrame(mktret), pd.DataFrame(xin_anret),
+                   left_index=True, right_index=True, how="inner")
+    rf = 1.036 ** (1/360) - 1
+    print(rf)
+    Eret = Ret - rf
+    print(Eret.head())
+    plt.scatter(Eret.values[:,0], Eret.values[:,1])
+    plt.title("XinAnGuFen return and market return")
+    plt.show()
+    model = sm.OLS(Eret.returns[1:], sm.add_constant(Eret.mktret[1:]))
+    result = model.fit()
+    print(result.summary())
