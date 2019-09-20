c05959e429193d4fad4d6d7f5ee14a7e5243673e
zhangxiaogui <zxglive2006@126.com>
2019-09-20 23:45:45 +0800

    完善19章内容

--- ch19.py
+++ ch19.py
@@ -4,7 +4,6 @@ import pandas as pd
 import math
 import matplotlib.pyplot as plt
 from scipy import linalg
-import ffn
 
 
 # 设置DataFrame显示宽度和列数
@@ -52,7 +51,8 @@ class MeanVariance:
         :param fracs:
         :return:
         """
-        meanRisky = ffn.to_returns(self.returns).mean()
+        from ffn import to_returns
+        meanRisky = to_returns(self.returns).mean()
         assert len(meanRisky) == len(fracs)     # Length of fractions must be equal to naumber of assets
         return np.sum(np.multiply(meanRisky, np.array(fracs)))
 
@@ -111,7 +111,7 @@ def plot_mean_sd():
     plt.show()
 
 
-def cal_corr():
+def get_sh_return():
     stock = pd.read_csv(r"./data/019/stock.txt", sep="\t", index_col="Trddt")
     byjc = stock.loc[stock.Stkcd == 600004, "Dretwd"]
     byjc.name = "byjc"
@@ -124,21 +124,85 @@ def cal_corr():
     zndl = stock.loc[stock.Stkcd == 600023, "Dretwd"]
     zndl.name = "zndl"
     sh_return = pd.concat([byjc, fjgs, hxyh, sykj, zndl], axis=1)
-    print(sh_return.head())
+    # print(sh_return.head())
     sh_return = sh_return.dropna()
-    print(sh_return.corr())
-    minVar = MeanVariance(sh_return)
-    # minVar.frontierCurve()
-    print(minVar.minVar(0.003))
+    sh_return.index = pd.to_datetime(sh_return.index)
+    # print(sh_return.corr())
     # cumreturn = (1 + sh_return).cumprod()
     # sh_return.plot()
     # plt.title("Daily Return of 5 Stocks(2014-2015)")
     # plt.legend(loc="lower center", ncol=5, fancybox=True, shadow=True)
     # plt.show()
     # cumreturn.plot()
-    # plt.show()
+    return sh_return
+
+
+def markowitz_model(_sh_return):
+    minVar = MeanVariance(_sh_return)
+    # minVar.frontierCurve()
+    print(minVar.minVar(0.003))
+    train_set = _sh_return["2014"]
+    test_set = _sh_return["2015"]
+    varMinimizer = MeanVariance(train_set)
+    goal_return = 0.003
+    portfolio_weight = varMinimizer.minVar(goal_return)
+    print(portfolio_weight)
+    temp = np.array([portfolio_weight[1,:].astype(np.float)]).swapaxes(0, 1)
+    print(temp)
+    test_return = np.dot(test_set, temp)
+    test_return = pd.DataFrame(test_return, index=test_set.index)
+    test_cum_return = (1 + test_return).cumprod()
+    sim_weight = np.random.uniform(0, 1, (100, 5))
+    sim_return = np.dot(test_set, sim_weight.swapaxes(0, 1))
+    sim_return = pd.DataFrame(sim_return, index=test_return.index)
+    sim_cum_return = (1 + sim_return).cumprod()
+    plt.plot(sim_cum_return.index, sim_cum_return, color='green')
+    plt.plot(test_cum_return.index, test_cum_return, color="red")
+    plt.show()
+
+
+def blacklitterman(returns, tau, P, Q):
+    mu = returns.mean()
+    sigma = returns.cov()
+    pi1 = mu
+    ts = tau * sigma
+    Omega = np.dot(np.dot(P, ts), P.T) * np.eye(Q.shape[0])
+    middle = linalg.inv(np.dot(np.dot(P, ts), P.T) + Omega)
+    er = np.expand_dims(pi1, axis=0).T + np.dot(np.dot(np.dot(ts, P.T), middle),
+                                                (Q - np.expand_dims(np.dot(P, pi1.T), axis=1)))
+    posteriorSigma = sigma + ts - np.dot(ts.dot(P.T).dot(middle).dot(P), ts)
+    return [er, posteriorSigma]
+
+
+def blminVar(blres, goalRet):
+    covs = np.array(blres[1])
+    means = np.array(blres[0])
+    L1 = np.append(np.append((covs.swapaxes(0, 1)), [means.flatten()], 0),
+                   [np.ones(len(means))], 0).swapaxes(0, 1)
+    L2 = list(np.ones(len(means)))
+    L2.extend([0, 0])
+    L3 = list(means)
+    L3.extend([0, 0])
+    L4 = np.array([L2, L3])
+    L = np.append(L1, L4, 0)
+    results = linalg.solve(L, np.append(np.zeros(len(means)), [1, goalRet], 0))
+    return (pd.DataFrame(results[:-2], index=blres[1].columns, columns=['p_weight']))
+
+
+def test_blacklitterman():
+    pick1 = np.array([1, 0, 1, 1, 1])
+    q1 = np.array([0.003 * 4])
+    pick2 = np.array([0.5, 0.5, 0, 0, -1])
+    q2 = np.array([0.001])
+    P = np.array([pick1, pick2])
+    Q = np.array([q1, q2])
+    print(P)
+    print(Q)
+    sh_return = get_sh_return()
+    res = blacklitterman(sh_return, 0.1, P, Q)
+    print(res)
 
 
 if __name__ == '__main__':
     print("ch19")
-    cal_corr()
+    test_blacklitterman()
